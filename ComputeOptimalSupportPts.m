90d8bc41e52899bcce7ac73706cddc83c60a1c23
Fred J Hickernell <hickernell@iit.edu>
2022-07-28 13:01:07 -0500

    Office Computer Update

--- ComputeOptimalSupportPts.m
+++ ComputeOptimalSupportPts.m
@@ -4,57 +4,65 @@ gail.InitializeWorkspaceDisplay
 
 kernelfun = @RoughMatern;
 ntry = 50; %number of gradient descent steps
-stepmultiply = [0.5 1 2]; %different step size multipliers
 
-nvec = 2.^(0:8);
+
+mmax = 8;
+nvec = 2.^(0:mmax);
+maxn=2^mmax;
 nveclength = length(nvec);
-finaldisc = zeros(nveclength,1);
+finaldisc(nveclength,1) = 0;
+initdisc(nveclength,1) = 0;
+deschange(nveclength,1) = 0;
+discimprov(nveclength,1) = 0;
+extdes = zeros(maxn,1);
+stopcrit =  1e-10;
+nov2 = 0;
+
 
 for jj = 1:nveclength
     n = nvec(jj);
     xdestry = zeros(n,3);
     xdes = (1:n)'/n - 1/(2*n);
+%     newpts = (xdes(1:nov2) + [0; xdes(1:nov2-1)])/2;
+%     xdes = [xdes(1:nov2,1) newpts];
     xdesinit = xdes;
     [sqdiscval,grad] = kernelfun(xdes,[1,1]); %initial squared discrepancy and gradient
-    sqdiscvalinit = sqdiscval;
+    initdisc(jj) = sqrt(sqdiscval);
     stepsize = 1/n;
     %disp('Beginning with a design of')
     %disp(xdes)
     %disp(['a squared discrepancy of ' num2str(sqdiscval,15)])
-    %disp(['a stepsize of ' num2str(stepsize)])(
-
-    for kk = 1:ntry
-        nominalstep = stepsize*grad; %how far to move
-        %compute new designs and their squared discrepancies
-        xdestry(:,1) = xdes - stepmultiply(1)*nominalstep;
-        sqdiscvaltry(1) = kernelfun(xdestry(:,1),[1,0]);
-        xdestry(:,2) = xdes - stepmultiply(2)*nominalstep;
-        sqdiscvaltry(2) = kernelfun(xdestry(:,2),[1,0]);
-        xdestry(:,3) = xdes - stepmultiply(3)*nominalstep;
-        sqdiscvaltry(3) = kernelfun(xdestry(:,3),[1,0]);
-        [sqdiscvalbest,wh] = min(sqdiscvaltry); %find the best disrepancy
-        if sqdiscvalbest < sqdiscval %we improved the design
-            xdes = xdestry(:,wh);
-            stepsize = stepmultiply(wh)*stepsize;
-            sqdiscval = sqdiscvalbest;
-        else %we did not improve the design
-            stepsize = 0.25*stepsize; %so take a smaller step
-        end
-        [~,grad] = kernelfun(xdes,[0,1]); %compute the new gradient
-    end
+    %disp(['a stepsize of ' num2str(stepsize)])
+    [xdes,sqdiscval,grad,stepsize] = ...
+        optimizeDesign(kernelfun,xdes,sqdiscval,grad,stepsize,ntry,stopcrit);
     finaldisc(jj) = sqrt(sqdiscval); %best discrepancy found
-    deschange = norm(xdes-xdesinit);
+    deschange(jj) = norm(xdes-xdesinit)/norm(xdesinit); %change in initial design
+    discimprov(jj) = finaldisc(jj)/initdisc(jj); %change in initial design
+%     nov2 = n;
     %disp('We end with a design and normalized gradient of')
     %disp([xdes,grad])
     %disp(['a squared discrepancy of ' num2str(sqdiscval,15)])
-    %disp(['  for a reduction of ' num2str(1-sqdiscval/sqdiscvalinit)])
+    %disp(['  for a reduction of ' num2str(1-sqdiscval/sqdiscvalinit(jj))])
     %disp(['a final stepsize of ' num2str(stepsize)])
-    disp(['relative l_2 design change ' num2str(deschange/norm(xdesinit))])
 end
 
-
+figure(1)
 loglog(nvec,finaldisc/finaldisc(1),'.',nvec,nvec(1)./nvec,'--')
+hold on
+loglog(nvec,initdisc/finaldisc(1),'o','markersize',10)
 xlabel('\(n\)')
-ylabel('Relative Discrepancy')
+ylabel('Normalized Discrepancy')
+
+figure
+loglog(nvec,discimprov,'.')
+xlabel('\(n\)')
+ylabel('Final Discrepancy Relative to Initial')
+
+figure
+loglog(nvec,deschange,'.')
+xlabel('\(n\)')
+ylabel('Relative \(\ell_2\) Design Change')
+
+
 
 
---
+++ optimizeDesign.m
@@ -0,0 +1,34 @@
+%% Optimize the design for integration given the kernel function
+function [xdes,sqdiscval,grad,stepsize] = ...
+    optimizeDesign(kernelfun,xdes,sqdiscval,grad,stepsize,ntry,stopcrit)
+stepmultiply = [0.5 1 2]; %different step size multipliers
+
+for kk = 1:ntry
+    nominalstep = stepsize*grad; %how far to move
+%     nominalstep = stepsize*grad(nov2+1:n); %how far to move
+    %compute new designs and their squared discrepancies
+    xdestry(:,1) = xdes - stepmultiply(1)*nominalstep;
+    sqdiscvaltry(1) = kernelfun(xdestry(:,1),[1,0]);
+    xdestry(:,2) = xdes - stepmultiply(2)*nominalstep;
+    sqdiscvaltry(2) = kernelfun(xdestry(:,2),[1,0]);
+    xdestry(:,3) = xdes - stepmultiply(3)*nominalstep;
+    sqdiscvaltry(3) = kernelfun(xdestry(:,3),[1,0]);
+%     xdestry(1:nov2,1:3) = xdes(1:nov2,[1 1 1]);
+%     xdestry(nov2+1:n,1) = xdes(nov2+1:n,1) - stepmultiply(1)*nominalstep;
+%     sqdiscvaltry(1) = kernelfun(xdestry(:,1),[1,0]);
+%     xdestry(:,2) = xdes - stepmultiply(2)*nominalstep;
+%     sqdiscvaltry(2) = kernelfun(xdestry(:,2),[1,0]);
+%     xdestry(:,3) = xdes - stepmultiply(3)*nominalstep;
+%     sqdiscvaltry(3) = kernelfun(xdestry(:,3),[1,0]);
+    [sqdiscvalbest,wh] = min(sqdiscvaltry); %find the best disrepancy
+    if sqdiscvalbest < sqdiscval %we improved the design
+        sqdiscimp = sqdiscval - sqdiscvalbest;
+        xdes = xdestry(:,wh);
+        stepsize = stepmultiply(wh)*stepsize;
+        sqdiscval = sqdiscvalbest;
+        if sqdiscimp/sqdiscval < stopcrit; break; end
+        [~,grad] = kernelfun(xdes,[0,1]); %compute the new gradient
+    else %we did not improve the design
+        stepsize = 0.25*stepsize; %so take a smaller step
+    end
+end
